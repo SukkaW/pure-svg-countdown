3ba13a028e95de2d3a4ad9dd9fcb0e5fd2655032
SukkaW <isukkaw@gmail.com>
2021-03-30 06:42:02 +0800

    Bring up the project

---
+++ src/api/contants.ts
@@ -0,0 +1,11 @@
+export const svgBegin = '<svg fill="none" viewBox="0 0 400 150" width="400" height="150" xmlns="http://www.w3.org/2000/svg">';
+export const svgForeignObjectBegin = '<foreignObject width="100%" height="100%">';
+export const svgContainerBegin = '<div xmlns="http://www.w3.org/1999/xhtml">';
+export const svgContainerEnd = '</div>';
+export const svgForeignObjectEnd = '</foreignObject>';
+export const svgEnd = '</svg>';
+
+export const mainStyle = `.main{background:#ededed;border:4px solid #000;height:142px}.divider{height:4px;background:#000;border:none;margin:0}.end,.timer,.title,.finish{text-align:center}.contain{position:relative;height:60px}.title{font-size:22px;line-height:1;padding:12px;font-weight:700}.timer,.finish{padding:12px;font-size:28px;position:absolute;width:368px}.timer>span:after{font-family:Consolas,Monaco,SFMono-Regular,Andale Mono,Liberation Mono,Ubuntu Mono,Menlo,monospace;width:0;animation-timing-function:step-end;animation-direction:normal;animation-play-state:running;animation-fill-mode:forwards;animation-iteration-count:infinite}.error{color:red}
+`;
+export const timerDisappearStyle = '@keyframes timer-disappear { 0% { opacity: 100% } 100% { opacity: 0% } }';
+export const finishAppearStyle = '@keyframes finish-appear { 0% { opacity: 0% } 100% { opacity: 100% } }';
---
+++ src/api/timer.ts
@@ -0,0 +1,116 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { finishAppearStyle, mainStyle, timerDisappearStyle, svgBegin, svgContainerBegin, svgContainerEnd, svgEnd, svgForeignObjectBegin, svgForeignObjectEnd } from './contants';
+
+export default async (request: VercelRequest, response: VercelResponse): Promise<void> => {
+  const { time, title, finish } = request.query;
+
+  response.setHeader('content-type', 'image/svg+xml');
+  response.status(200).send(buildSvg(time, title, finish));
+};
+
+const padNumber = (num: number): string => String(num).padStart(2, '0');
+
+function buildSvg(time: string | string[], title: string | string[] = 'Countdown', finish: string | string[] = 'The auction has expired'): string {
+  if (Array.isArray(time)) time = time.join('');
+  if (Array.isArray(title)) title = title.join(' ');
+
+  let style = '';
+  let timerDiv = '';
+
+  try {
+    const endDate = new Date(time);
+    const end = endDate.getTime();
+    const now = new Date().getTime();
+
+    let seconds = (end - now) / 1000;
+    const allSecondsLeft = Math.round(seconds);
+
+    if (allSecondsLeft > 0) {
+      const daysLeft = Math.floor(allSecondsLeft / 86400);
+      seconds = allSecondsLeft - daysLeft * 86400;
+      const daysDelay = seconds;
+
+      const hoursLeft = Math.floor(seconds / 3600);
+      seconds = seconds - hoursLeft * 3600;
+      const hoursDelay = seconds;
+
+      const minutesLeft = Math.floor(seconds / 60);
+      seconds = seconds - minutesLeft * 60;
+      const secondsLeft = Math.round(seconds);
+      const minutesDelay = secondsLeft;
+
+      style += `.seconds::after { content: "${secondsLeft}"; animation-name: countdown-seconds; animation-duration: 60s; }`;
+      style += `.minutes::after { content: "${minutesLeft}"; animation-name: countdown-minutes; animation-duration: 3600s; animation-delay: ${minutesDelay}s }`;
+      style += `.hours::after { content: "${hoursLeft}"; animation-name: countdown-hours; animation-duration: 86400s; animation-delay: ${hoursDelay}s }`;
+      style += `.days::after { content: "${daysLeft}"; animation-name: countdown-days; animation-duration: ${daysLeft * 86400}s; animation-delay: ${daysDelay}s }`;
+      style += `.timer { opacity: 100%; animation-name: timer-disappear; animation-duration: 1s; animation-timing-function:step-end;animation-direction: normal; animation-play-state: running; animation-fill-mode: forwards; animation-delay: ${allSecondsLeft - 1}s }`;
+      style += `.finish { opacity: 0%; animation-name: finish-appear; animation-duration: 1s; animation-timing-function:step-end; animation-direction: normal; animation-play-state: running; animation-fill-mode: forwards; animation-delay: ${allSecondsLeft - 1}s }`;
+
+      style += createKeyframes(secondsLeft - 1, 60, 'seconds');
+      style += createKeyframes(minutesLeft - 1, 60, 'minutes');
+      style += createKeyframes(hoursLeft - 1, 24, 'hours');
+      style += createKeyframes(daysLeft - 1, daysLeft, 'days');
+      style += timerDisappearStyle + finishAppearStyle;
+
+      timerDiv += '<div class="contain">';
+      timerDiv += '<div class="timer">';
+      timerDiv += '<span class="days"></span>d ';
+      timerDiv += '<span class="hours"></span>h ';
+      timerDiv += '<span class="minutes"></span>m ';
+      timerDiv += '<span class="seconds"></span>s';
+      timerDiv += '</div>';
+      timerDiv += `<div class="finish"><span class="error">${finish}</span></div>`;
+      timerDiv += '</div>';
+    } else {
+      timerDiv = `<div class="contain"><div class="timer"><span class="error">${finish}</span></div></div>`;
+    }
+
+    timerDiv += `<div class="end">End: ${formatTime(endDate)}</div>`;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    timerDiv = '<div class="timer"><span class="error">There is something goes wrong here!</span></div>';
+  }
+
+  let result = '';
+
+  result += svgBegin;
+  result += svgForeignObjectBegin;
+  result += '<style>';
+  result += mainStyle + style;
+  result += '</style>';
+  result += svgContainerBegin;
+  result += '<div class="main">';
+  result += `<div class="title">${title}</div>`;
+  result += '<hr class="divider" />';
+
+  result += timerDiv;
+
+  result += '</div>';
+  result += svgContainerEnd;
+  result += svgForeignObjectEnd;
+  result += svgEnd;
+
+  return result;
+}
+
+function formatTime(date: Date): string {
+  return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()} ${padNumber(date.getUTCHours())}:${padNumber(date.getUTCMinutes())}:${padNumber(date.getUTCSeconds())} UTC`;
+}
+
+function createKeyframes(start: number, steps: number, name: string): string {
+  let result = `@keyframes countdown-${name} {`;
+
+  for (let i = 0; i < steps; i++) {
+    result += `${((100 / steps) * i).toFixed(2)}%{`;
+    result += `content: "${start - i >= 0 ? start - i : steps + start - i}"`;
+    result += '}';
+  }
+
+  result += '100%{';
+  result += `content:"${start - steps + 1 >= 0 ? start - steps + 1 : start + 1}"`;
+  result += '}';
+
+  result += '}';
+  return result;
+}
